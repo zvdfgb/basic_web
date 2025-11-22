fa5b1c5e75913389506870d1308b1c2297cd1856
zvdfgb <2727999431@qq.com>
2025-11-22 17:54:31 +0800

    完成了注册功能

---
+++ mauth/forms.py
@@ -0,0 +1,38 @@
+from django import forms
+from django.contrib.auth import get_user_model
+from .models import Captcha
+
+User = get_user_model()
+
+class RegisterForm(forms.Form):
+    username= forms.CharField(label='Username',max_length=20,min_length=2,error_messages={
+        'required':'请输入你的用户名!',
+        'max_length':'用户名在2~20之间!',
+        'min_length':'用户名在2~20之间!',
+    })
+    email = forms.EmailField(label='Email',error_messages={
+        'required':'请输入邮箱！',
+        'invalid':'请输入正确的邮箱！',
+    })
+    captcha = forms.CharField(label='Captcha',max_length=4,min_length=4,error_messages={
+        'required':'请输入验证码！',
+        'max_length':'请输入四位验证码！',
+        'min_length':'请输入四位验证码！',
+    })
+    password = forms.CharField(label='Password',max_length=20,min_length=6)
+
+    def clean_email(self):
+        email = self.cleaned_data.get['email']
+        exists = User.objects.filter(email=email).exists()
+        if exists:
+            raise forms.ValidationError('邮箱已经注册！')
+        return email
+
+    def clean_captcha(self):
+        captcha = self.cleaned_data.get('captcha')
+        email = self.cleaned_data.get['email']
+        captcha_model = Captcha.objects.filter(email=email,captcha=captcha).first()
+        if not captcha:
+            raise forms.ValidationError("验证码与邮箱不匹配！")
+        captcha_model.delete()
+        return captcha
--- mauth/views.py
+++ mauth/views.py
@@ -1,19 +1,38 @@
 from email.policy import default
 
-from django.shortcuts import render
+from django.shortcuts import render,redirect,reverse
 from django.http.response import JsonResponse
 import string
 import random
 from django.core.mail import send_mail
-from .models import Captcah
+from .models import Captcha
+from django.views.decorators.http import require_http_methods
+from .forms import RegisterForm
+from django.contrib.auth import get_user_model
 # Create your views here.
 
+User = get_user_model()
 
 def login(request):
     return render(request,'login.html')
 
+@require_http_methods(['GET','POST'])
 def register(request):
-    return render(request,'register.html')
+    if request.method == 'GET':
+        return render(request,'register.html')
+    else:
+        form = RegisterForm(request.POST)
+        if form.is_valid():
+            username=form.cleaned_data.get('username')
+            email = form.cleaned_data.get('email')
+            password = form.cleaned_data.get('password')
+            User.objects.create_user(email=email,username=username,password=password)
+            return redirect(reverse('mauth:login'))
+        else:
+            print(form.errors)
+            #重新跳转到注册页面
+            return redirect(reverse('mauth:register'))
+            # return render(request,'register.html',context={'form':form})
 
 def send_email_captcha(request):
     #?email=xxx的形式访问
@@ -23,6 +42,6 @@ def send_email_captcha(request):
     #生成验证码（取四位阿拉伯数字
     captcah="".join(random.sample(string.digits,4))
     #储存到数据库中
-    Captcah.objects.update_or_create(email=email,defaults={'captcha':captcah})
+    Captcha.objects.update_or_create(email=email,defaults={'captcha':captcah}  )
     send_mail("博客注册验证码",message=f"您的注册验证码为：{captcah}",recipient_list=[email],from_email=None)
     return JsonResponse({"code":200,"message":"邮箱验证码发送成功！"})
